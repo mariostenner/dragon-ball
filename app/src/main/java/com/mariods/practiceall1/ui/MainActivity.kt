836f6634af4a661b6f17bdfb1ec03b8fd9578874
Mario DS <mariostenner@gmail.com>
2024-10-11 18:38:58 -0600

    Se agrega configuracion en scaffold

--- app/src/main/java/com/mariods/practiceall1/ui/MainActivity.kt
+++ app/src/main/java/com/mariods/practiceall1/ui/MainActivity.kt
@@ -5,26 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.mariods.practiceall1.ui.theme.PracticeAll1Theme
 import com.mariods.practiceall1.ui.view.home.HomeScreen
-
 import com.mariods.practiceall1.ui.view.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity (): ComponentActivity() {
+class MainActivity() : ComponentActivity() {
     private val dragonViewModel: HomeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PracticeAll1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(dragonViewModel)
-                }
+                HomeScreen(dragonViewModel)
             }
         }
     }
