b476342fa545e956e395c490326ad57f40797202
Mario DS <mariostenner@gmail.com>
2024-10-14 17:47:46 -0600

    Se cambia a query la llamda en retrofit

--- app/src/main/java/com/mariods/practiceall1/data/network/DragonApiClient.kt
+++ app/src/main/java/com/mariods/practiceall1/data/network/DragonApiClient.kt
@@ -5,11 +5,13 @@ import com.mariods.practiceall1.data.model.TransformationsNetworkModel
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface DragonApiClient {
 
-    @GET("/api/characters?page=1&limit=1000")
-    suspend fun getCharacters(): Response<CharacterNetworkItems>
+    @GET("/api/characters")
+    suspend fun getCharacters(@Query("page") page: Int = 1,
+                              @Query("limit") limit: Int = 1000): Response<CharacterNetworkItems>
 
     @GET("/api/characters/{id}")
     suspend fun getDetailCharacter(@Path("id") id: Int): Response<TransformationsNetworkModel>
